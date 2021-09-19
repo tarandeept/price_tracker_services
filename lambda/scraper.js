e0dc933d2d025f0637896e7e1e80c1316b173668
Tarandeep Singh Thamrait <tthamrai@uci.edu>
2021-09-18 22:29:42 -0700

    Added npm package for lambdas

--- lambda/scraper.js
+++ lambda/scraper.js
@@ -1,8 +1,11 @@
-const aws = require('aws-sdk');
+const cheerio = require('cheerio');
 
 exports.handler = async function(event) {
   console.log("EVENT: ", event);
   console.log("PRODUCT URL: ", event.product_url);
+
+  const $ = cheerio.load('<ul id="fruits">...</ul>', null, false);
+  console.log($.html());
 
   return {
     statusCode: 200,
