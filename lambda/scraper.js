551fe4f1735502d1441f1c43f8fcafc94b434a47
Tarandeep Singh Thamrait <tthamrai@uci.edu>
2021-09-26 12:19:57 -0700

    Added got scraping

--- lambda/scraper.js
+++ lambda/scraper.js
@@ -1,5 +1,5 @@
 const cheerio = require('cheerio');
-const got = require('got');
+const gotScraping = require('got-scraping');
 const { build_response } = require("./utils");
 
 const extract_price = (html) => {
@@ -16,8 +16,23 @@ const extract_title = (html) => {
 
 exports.handler = async function(event) {
   try {
-    const product_url = event.product_url;
-    const response = await got(product_url);
+    // const product_url = event.product_url;
+    const product_url = 'https://www.amazon.com/Jura-Tumbler-Double-Stainless-Installation/dp/B07MJR3P1H/ref=sr_1_5?dchild=1&keywords=coffee+cup&qid=1632639304&sr=8-5';
+
+    const headers = {
+      'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246',
+      'content-type': 'application/json',
+      'accept': 'text/html, */*; q=0.01',
+      'accept-language': 'en-US,en;q=0.9',
+      'x-requested-with': 'XMLHttpRequest',
+    }
+
+    const response = await gotScraping({
+      url: product_url,
+      headers: headers
+    });
+
+    console.log(response.body);
 
     const html = cheerio.load(response.body, null, false);
     const price = extract_price(html);
@@ -30,3 +45,9 @@ exports.handler = async function(event) {
     return build_response(400, JSON.stringify(body));
   }
 };
+
+const main = async () => {
+  await exports.handler('event');
+}
+
+main()
