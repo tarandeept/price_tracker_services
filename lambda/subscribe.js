b118a63b5502ce425a6a4f8abfdb9915303998e9
Tarandeep Singh Thamrait <tthamrai@uci.edu>
2021-10-19 23:17:27 -0700

    Added function to check if email is already subscribed

--- lambda/subscribe.js
+++ lambda/subscribe.js
@@ -2,29 +2,41 @@ const { DynamoDB } = require('aws-sdk');
 const { randomUUID } = require('crypto');
 const { build_response } = require("./utils");
 
-const subscribe_user = (product_url, email, target_price) => {
+
+const is_subscribed = async (product_url, email) => {
   const dynamo = new DynamoDB();
-  const id = randomUUID();
 
   var params = {
     TableName: process.env.TABLE_NAME,
-    KeyConditionExpression: 'product_url = :url',
+    IndexName: 'emailIndex',
+    KeyConditionExpression: 'email = :email',
+    FilterExpression: 'product_url = :product_url',
     ExpressionAttributeValues: {
-      ':url': { S: product_url }
+      ':email': { S: email },
+      ':product_url': { S: product_url }
     }
   }
 
   const data = await dynamo.query(params).promise();
+  return data.Count >= 1;
+}
+
+const subscribe_user = async (product_url, email, target_price) => {
+  subscribed = await is_subscribed(product_url, email);
+
+  if (!subscribed) {
+    const id = randomUUID();
+  }
 }
 
 exports.handler = async function(event) {
   try {
-    console.log('EVENT: ', JSON.stringify(event));
+    // console.log('EVENT: ', JSON.stringify(event));
 
     const body = JSON.parse(event.body);
     const { product_url, email, target_price } = body;
 
-    subscribe_user(product_url, email, target_price);
+    await subscribe_user(product_url, email, target_price);
 
     return build_response(200, 'Successfully subscribed user to product');
   } catch(error) {
